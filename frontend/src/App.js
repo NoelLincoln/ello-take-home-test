09ee71359c965d178396241fe99b6449ca75afa6
Noel Bryant <noelsobryant@gmail.com>
2024-06-07 16:29:49 +0300

    Create Search component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <header className="App-header">
       <Header/>
       </header>
+      <SearchBar/>
+
     </div>
   );
 }
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TextField, Button, Box } from '@mui/material';
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [searchText, setSearchText] = useState('');
 
-export default SearchBar
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log('Searching for:', searchText);
+  };
+
+  return (
+    <Box display="flex" width="100%" justifyContent="center" paddingTop={2} overflow="visible">
+      <Box
+        display="flex"
+        alignItems="center"
+        borderRadius="20px"
+        overflow="hidden"
+        sx={{ position: 'relative' }}
+      >
+        <TextField
+          label="Search"
+          variant="outlined"
+          value={searchText}
+          onChange={handleSearchChange}
+          style={{ marginRight: '8px', width: '80%', borderRadius: 20 }}
+          InputProps={{ sx: { borderRadius: 20 } }}
+        />
+        <Button
+        variant="contained"
+        onClick={handleSearch}
+        style={{ borderRadius: 20 }}
+         >
+        Search
+      </Button> 
+     </Box>
+    </Box>
+  );
+  
+  
+  
+};
+
+export default SearchBar;
