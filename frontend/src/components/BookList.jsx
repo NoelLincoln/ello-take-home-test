90b9a64fdf3bf60beefec58f4cfa932dde515372
Noel Bryant <noelsobryant@gmail.com>
2024-06-07 21:47:23 +0300

    Add descriptive title in the homepage.

--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Grid from '@mui/material/Grid';
-
+import { Typography } from '@mui/material';
 
 const GET_BOOKS = gql`
   query ExampleQuery {
@@ -24,16 +24,19 @@ const BookList = () => {
   const sortedBooks = [...data.books].sort((a, b) => a.title.localeCompare(b.title));
 
   return (
+    <>
+    <Typography variant="h4" align="center" gutterBottom>List of all Books</Typography>
     <Grid container spacing={2} justifyContent="center" sx={{ mx: 'auto', width: '80%' }}>
-    {sortedBooks.map(book => (
-      <Grid key={book.title} item xs={12} sm={6} md={4} lg={3}>
-        <div style={{ textAlign: 'center' }}>
-          <img src={book.coverPhotoURL} alt={book.title} style={{ width: '100%' }} />
-          <p>{book.title}</p>
-        </div>
-      </Grid>
-    ))}
-  </Grid>
+      {sortedBooks.map(book => (
+        <Grid key={book.title} item xs={12} sm={6} md={4} lg={3}>
+          <div style={{ textAlign: 'center' }}>
+            <img src={book.coverPhotoURL} alt={book.title} style={{ width: '100%' }} />
+            <p>{book.title}</p>
+          </div>
+        </Grid>
+      ))}
+    </Grid>
+  </>
   );
 };
 
