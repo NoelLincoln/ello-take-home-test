59093a9b8bf6757241a73a7ac849a26869da6c73
Noel Bryant <noelsobryant@gmail.com>
2024-06-08 05:01:44 +0300

    Define GraphQL query for fetching paginated book data
    - Create GET_ALL_BOOKS query using Apollo Client's gql function
    - Include offset and limit arguments to support pagination
    - Retrieve title, author, and coverPhotoURL fields for each book

--- frontend/src/utils/queries.js
+++ frontend/src/utils/queries.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const GET_ALL_BOOKS = gql`
-  query GetAllBooks {
-    books {
+  query GetAllBooks($offset: Int, $limit: Int) {
+    books(offset: $offset, limit: $limit) {
       title
       author
       coverPhotoURL
