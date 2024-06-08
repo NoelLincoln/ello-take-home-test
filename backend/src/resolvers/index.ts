dc3bc08276acf9c64fdb585d5f2837ba1e7d9228
Noel Bryant <noelsobryant@gmail.com>
2024-06-08 10:18:41 +0300

    Add custom query for displaying all books in the search field.

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -1,13 +1,15 @@
 import { booksData } from '../data/books';
-import { QueryBooksArgs } from '../types';
 
 export const resolvers = {
   Query: {
-    books: (_: unknown, { offset = 0, limit = 10 }: QueryBooksArgs) => {
+    books: (_: unknown, { offset = 0, limit = 10 }: { offset: number; limit: number }) => {
       const sortedBooks = booksData.sort((a, b) => a.title.localeCompare(b.title));
       const startIndex = offset;
       const endIndex = offset + limit;
       return sortedBooks.slice(startIndex, endIndex);
+    },
+    books_search: () => {
+      return booksData.sort((a, b) => a.title.localeCompare(b.title));
     },
   },
 };
