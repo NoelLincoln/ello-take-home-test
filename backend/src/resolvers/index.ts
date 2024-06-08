6394fa15fbae1cac510c5fe33d329ae918d0142d
Noel Bryant <noelsobryant@gmail.com>
2024-06-08 05:15:54 +0300

    Sort books alphabetically before lazy loading.

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -4,9 +4,10 @@ import { QueryBooksArgs } from '../types';
 export const resolvers = {
   Query: {
     books: (_: unknown, { offset = 0, limit = 10 }: QueryBooksArgs) => {
+      const sortedBooks = booksData.sort((a, b) => a.title.localeCompare(b.title));
       const startIndex = offset;
       const endIndex = offset + limit;
-      return booksData.slice(startIndex, endIndex);
+      return sortedBooks.slice(startIndex, endIndex);
     },
   },
 };
